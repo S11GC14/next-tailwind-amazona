057ab06e16140a59642dbe2c7f0d1273062bb76e
suchithWZB14 <suchith.g@wyzebulb.com>
2022-11-24 13:18:19 +0530

    feat:

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ import ProductItem from '../components/ProductItem';
 import db from '../utils/db';
 import { Store } from '../utils/Store';
 
-export default function Home({ products }) {
+export default function Home({ products }: any) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
 
-  const addToCartHandler = async (product) => {
-    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+  const addToCartHandler = async (product: any) => {
+    const existItem = cart.cartItems.find((x: any) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
 
@@ -27,7 +27,7 @@ export default function Home({ products }) {
   return (
     <Layout title="Home page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((product) => (
+        {products.map((product: any) => (
           <ProductItem
             product={product}
             key={product.slug}
